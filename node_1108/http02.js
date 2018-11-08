1949b8402ca188d91ff692b28c5fa55181f0a939
markim94 <markim94@naver.com>
2018-11-08 20:20:46 +0900

    http2

--- node_1108/http02.js
+++ node_1108/http02.js
@@ -6,8 +6,9 @@ var myServer = http.createServer();
 
 // 3000번 포트 지정
 var connectPort = 3000;
+var connectIP = '192.168.1.66'
 
 // myServer에 listen을 이용하여 서버를 시작하는 것.
-myServer.listen(connectPort, function(){
-    console.log('웹서버가 시작되었습니다 : %d', connectPort);
+myServer.listen(connectPort, connectIP, '50000', function(){
+    console.log('웹서버가 시작되었습니다 : %s, %d', connectIP, connectPort);
 });
