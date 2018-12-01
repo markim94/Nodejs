cf788485c038a73e1dd2470badd996bd6d086336
markim94 <markim94@naver.com>
2018-12-01 17:38:29 +0900

    add router middleware in toyproject at nonejs

--- node_1201/expressExample/server.js
+++ node_1201/expressExample/server.js
@@ -5,6 +5,9 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var static = require('serve-static');
 
+// 라우터 객체 참조.
+var router = express.Router();
+// express 객체 생성
 var app = express();
 
 app.set('port', process.env.PORT||3000); //고유키워드, 포트번호(process.env의 port번호가 있으면 이 것을 사용 그렇지 않다면 3000설정)
@@ -20,9 +23,11 @@ app.use(bodyParser.json());
 // 앞에 '/' : 원하는 경로 지정, 뒤에 'public' : 폴더명
 app.use('/', static(path.join(__dirname, 'public')));
 
-app.use(function(req,res, next)
+// 라우팅 함수 등록하고, /precess/login으로 요청 패스를 처리함
+// post방식으로 요청할 때는 post방식으로
+router.route('/process/login').post(function(req,res)
 {
-    console.log('첫 번째 미들웨어에서 요청을 처리함');
+    console.log('/process/login 처리함');
     
     //클라이언트에서 요청할 때, GET방식으로 요청할지 POST방식으로 요청할지 모르는 상태
     var paramId = req.body.id || req.query.id;
@@ -30,11 +35,15 @@ app.use(function(req,res, next)
 
     // 응답 전송
     res.writeHead('200', {'Content-Type':'text/html;charset=utf-8'});
-    res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+    res.write('<h1>Express 서버에서 응답한 미들웨어 사용 결과입니다.</h1>');
     res.write('<div><p>Param ID :'+paramId+'</p></div>');
     res.write('<div><p>Param Password :'+paramPassword+'</p></div>');
+    res.write("<br><br><a href='/login1.html'>로그인 페이지로 돌아가기</a>");
     res.end();
 });
+
+// 라우터 객체를 app객체에 등록함.
+app.use('/', router);
 
 //Express 객체를 받아와 3000번 포트에서 서버 시작.
 http.createServer(app).listen(3000,function()
